fc9a1727b5361d38f0deb7ff4fc58222c8270911
Aditya Bimawan <bimawan.aditya@moneyforward.co.jp>
2024-12-05 17:07:16 +0900

    whatever

--- assets/js/ball.js
+++ assets/js/ball.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -23,6 +24,25 @@ points.push(new THREE.Vector3(0, 5, 0));
 points.push(new THREE.Vector3(5, 0, 0));
 const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
 const line = new THREE.Line(lineGeometry, lineMaterial);
+const light = new THREE.AmbientLight(0x404040, 100); // soft white light
+scene.add(light);
+
+var beerBottle;
+const loader = new GLTFLoader();
+loader.load(
+    "assets/gltf/beer_bottle/scene.gltf",
+    function (gltf) {
+        beerBottle = gltf;
+
+        beerBottle.scene.scale.setScalar(0.1);
+        beerBottle.scene.position.x = 3;
+        scene.add(beerBottle.scene);
+    },
+    undefined,
+    function (error) {
+        console.log(error);
+    }
+);
 
 scene.add(cube);
 scene.add(line);
@@ -32,6 +52,11 @@ camera.position.x = 2;
 camera.position.y = -1;
 
 function animate() {
+    if (beerBottle) {
+        beerBottle.scene.rotation.x += 0.02;
+        beerBottle.scene.rotation.y += 0.02;
+        beerBottle.scene.rotation.z += 0.02;
+    }
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
     renderer.render(scene, camera);
